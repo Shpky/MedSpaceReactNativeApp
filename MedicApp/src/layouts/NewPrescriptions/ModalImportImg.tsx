f41806db000f830ed2f18a639d21384a3b6eb385
Romain <romain.gouraud@etu.univ-nantes.fr>
2023-11-22 08:24:33 +0100

    update modale et détection médicament +ocr

--- MedicApp/src/layouts/NewPrescriptions/ModalImportImg.tsx
+++ MedicApp/src/layouts/NewPrescriptions/ModalImportImg.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { Alert, Modal, StyleSheet, Text, Pressable, View, Image, Button } from 'react-native';
 import { launchCamera, launchImageLibrary, CameraOptions, ImageLibraryOptions } from 'react-native-image-picker';
-import TextRecognition from '@react-native-ml-kit/text-recognition';
+import TextRecognition, { TextRecognitionResult } from '@react-native-ml-kit/text-recognition';
+import DetectMedicineinsentence from '@layouts/NewPrescriptions/ImportImg/MedicineRecognitionWE';
 
 const ModalImgPicker = ({ setprescription }: { setprescription: Dispatch<SetStateAction<PrescriptionInterface>> }) => {
     const [image, setImage] = useState<string | null>(null);
@@ -10,7 +11,11 @@ const ModalImgPicker = ({ setprescription }: { setprescription: Dispatch<SetStat
     const TextRecognitionContainer = async (input: string) => {
         const result = await TextRecognition.recognize(input);
         console.log('Recognized text:', result.text);
-        for (let block of result.blocks) {
+        console.log(typeof (result.text));
+
+        
+
+        /*for (let block of result.blocks) {
             console.log('Block text:', block.text);
             console.log('Block frame:', block.frame);
 
@@ -19,144 +24,148 @@ const ModalImgPicker = ({ setprescription }: { setprescription: Dispatch<SetStat
                 console.log('Line frame:', line.frame);
             }
 
-        }}
+        }*/
+    }
+
+    const DetectMedicineContainer = async (input:TextRecognitionResult ) => {
+        let Medecines = DetectMedicineinsentence(input)
+    }
 
 
 
-
-        const cameraHandler = () => {
-            const options: CameraOptions = {
-                mediaType: 'photo',
-            }
-            launchCamera(options, async (response) => {
-                if (response.assets && response.assets[0] && typeof response.assets[0].uri === "string") {
-                    setImage(response.assets[0].uri);
-                    if (response.assets[0].uri) { TextRecognitionContainer(response.assets[0].uri); setModalVisible(!modalVisible); }
-
-                }
-            })
-        };
-
-
-
-        const libraryHandler = async () => {
-            const options: ImageLibraryOptions = {
-                mediaType: 'photo',
-                selectionLimit: 1,
-            };
-            launchImageLibrary(options, async (response) => {
-                if (response.assets && response.assets[0] && typeof response.assets[0].uri === "string") {
-                    setImage(response.assets[0].uri);
-                    if (response.assets[0].uri) { TextRecognitionContainer(response.assets[0].uri); setModalVisible(!modalVisible); }
-
-                }
-            });
+    const cameraHandler = () => {
+        const options: CameraOptions = {
+            mediaType: 'photo',
         }
+        launchCamera(options, async (response) => {
+            if (response.assets && response.assets[0] && typeof response.assets[0].uri === "string") {
+                setImage(response.assets[0].uri);
+                if (response.assets[0].uri) { TextRecognitionContainer(response.assets[0].uri); setModalVisible(!modalVisible); }
 
-
-
-
-
-        return (
-            <View style={styles.centeredView}>
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        Alert.alert('Modal has been closed.');
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                            <Text style={styles.modalText}> Que voulez vous faire ?</Text>
-                            <View style={{ flexDirection: 'row', }}>
-                                <Pressable
-                                    style={[styles.button, styles.buttonGallery]}
-                                    onPress={() => libraryHandler()}>
-                                    <Text >Gallery</Text></Pressable>
-                                <Pressable
-                                    style={[styles.button, styles.buttonAppPhoto]}
-                                    onPress={() => cameraHandler()}>
-                                    <Text >Appareil Photo</Text></Pressable>
-
-                            </View>
-                            <Pressable
-                                style={[styles.button, styles.buttonClose]}
-                                onPress={() => setModalVisible(!modalVisible)}>
-                                <Text style={styles.textStyle}>Fermez</Text>
-                            </Pressable>
-                        </View>
-                    </View>
-                </Modal>
-                <Pressable
-                    style={[styles.button, styles.buttonOpen]}
-                    onPress={() => setModalVisible(true)}>
-                    <Text style={styles.textStyle}>Voulez vous scannez votre ordonnace ?</Text>
-                </Pressable>
-            </View>
-        );
+            }
+        })
     };
 
-    const styles = StyleSheet.create({
-        centeredView: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 22,
-        },
-        modalView: {
-            margin: 20,
-            backgroundColor: 'white',
-            borderRadius: 20,
-            padding: 35,
-            alignItems: 'center',
-            shadowColor: '#000',
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
-            elevation: 5,
-        },
-        buttonGallery: {
-            marginRight: 10,
-            borderRadius: 20,
-            padding: 10,
-            elevation: 2,
-            backgroundColor: '#F194FF',
-            justifyContent: 'center'
-        },
-        buttonAppPhoto: {
-            borderRadius: 20,
-            padding: 10,
-            elevation: 2,
-            backgroundColor: '#F194FF',
-            justifyContent: 'center'
-        }
-        ,
-        button: {
-            borderRadius: 20,
-            padding: 10,
-            elevation: 2,
-        },
-        buttonOpen: {
-            backgroundColor: '#F194FF',
-        },
-        buttonClose: {
-            backgroundColor: '#FF0000',
-            marginTop: 10,
-        },
-        textStyle: {
-            color: 'white',
-            fontWeight: 'bold',
-            textAlign: 'center',
-        },
-        modalText: {
-            marginBottom: 15,
-            textAlign: 'center',
-        },
-    });
 
-    export default ModalImgPicker;
+
+    const libraryHandler = async () => {
+        const options: ImageLibraryOptions = {
+            mediaType: 'photo',
+            selectionLimit: 1,
+        };
+        launchImageLibrary(options, async (response) => {
+            if (response.assets && response.assets[0] && typeof response.assets[0].uri === "string") {
+                setImage(response.assets[0].uri);
+                if (response.assets[0].uri) { TextRecognitionContainer(response.assets[0].uri); setModalVisible(!modalVisible); }
+
+            }
+        });
+    }
+
+
+
+
+
+    return (
+        <View style={styles.centeredView}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}> Que voulez vous faire ?</Text>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Pressable
+                                style={[styles.button, styles.buttonGallery]}
+                                onPress={() => libraryHandler()}>
+                                <Text >Gallery</Text></Pressable>
+                            <Pressable
+                                style={[styles.button, styles.buttonAppPhoto]}
+                                onPress={() => cameraHandler()}>
+                                <Text >Appareil Photo</Text></Pressable>
+
+                        </View>
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}>
+                            <Text style={styles.textStyle}>Fermez</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Pressable
+                style={[styles.button, styles.buttonOpen]}
+                onPress={() => setModalVisible(true)}>
+                <Text style={styles.textStyle}>Voulez vous scannez votre ordonnace ?</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    buttonGallery: {
+        marginRight: 10,
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: '#F194FF',
+        justifyContent: 'center'
+    },
+    buttonAppPhoto: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: '#F194FF',
+        justifyContent: 'center'
+    }
+    ,
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#FF0000',
+        marginTop: 10,
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+});
+
+export default ModalImgPicker;
